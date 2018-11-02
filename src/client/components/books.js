9baef6d7f9117d96a6b360f032c7cbebd18683b8
abby wigdale <abby.wigdale@gmail.com>
2018-11-01 19:21:58 -0500

    added input form for search bar

--- src/client/components/books.js
+++ src/client/components/books.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react'
-
+import React, { Component, Fragment } from 'react';
 
 class Books extends Component {
   state = {
-    allBooks: []
+    allBooks: [],
+    searchInput: '',
+  };
+
+  handleChange(event) {
+    this.setState({ searchInput: event.target.value });
   }
 
+  handleSubmit(event) {
+    //make api request
+  }
 
   render() {
     return (
-      <h1>Books</h1>
-    )
+      <Fragment>
+        <h1>Books</h1>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <label>
+            search:
+            <input
+              type="text"
+              value={this.state.searchInput}
+              onChange={this.handleChange.bind(this)}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </Fragment>
+    );
   }
-
 }
 
-export default Books
+export default Books;
