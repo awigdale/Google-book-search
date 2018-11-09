065367cac45e3fde53dd5aeb1a5a5aac9f1f8cd7
abby wigdale <abby.wigdale@gmail.com>
2018-11-09 11:34:51 -0800

    extract hardcodedstartIndex and maxResults

--- src/client/api.js
+++ src/client/api.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 export async function handle_GoogleBooks_API_Request(searchInput) {
   try {
+    let startIndex = 0;
+    let maxResults = 15;
     const { data } = await axios.get(
-      `https://www.googleapis.com/books/v1/volumes?q=${searchInput}`
+      `https://www.googleapis.com/books/v1/volumes?q=${searchInput}&startIndex=${startIndex}&maxResults=${maxResults}`
     );
     return data;
   } catch (err) {
